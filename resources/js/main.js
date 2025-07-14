34938773b26ecb18cd8db282e5a38d2c29846780
nomennescioserbia <otvoreninalog1@gmail.com>
2025-07-14 21:50:03 +0200

    Add files via upload

---
+++ resources/js/main.js
@@ -0,0 +1,9 @@
+
+function switchCyrillic() {
+  document.body.classList.remove('latin');
+  document.body.classList.add('cyrillic');
+}
+function switchLatin() {
+  document.body.classList.remove('cyrillic');
+  document.body.classList.add('latin');
+}
